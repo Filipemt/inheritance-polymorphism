532c91b639fa3cca25ce7b54fbec98d8f3066d1e
Filipemtb <filipera3@gmail.com>
2024-03-13 15:27:10 -0300

    fix error
    corrigindo o erro que foi simulado utilizando o modificador de acesso "protected"

--- introducaoHeranca/src/entities/Account.java
+++ introducaoHeranca/src/entities/Account.java
@@ -2,7 +2,7 @@ package entities;
 public class Account {
     private Integer number;
     private String holder;
-    private Double balance;
+    protected Double balance;
 
     public Account() {}
 
