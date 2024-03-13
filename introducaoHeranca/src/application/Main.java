2f772f9d66fe7fb1e45e1c1e9434e30e5a7f1c5f
Filipemtb <filipera3@gmail.com>
2024-03-13 15:21:51 -0300

    create: add class Account and BusinessAccount
    classes criadas para aprender sobre Herança

--- introducaoHeranca/src/application/Main.java
+++ introducaoHeranca/src/application/Main.java
@@ -1,7 +1,12 @@
 package application;
 
+import entities.BusinessAccount;
+
 public class Main {
     public static void main(String[] args) {
+
+        BusinessAccount account = new BusinessAccount();
+        account.getNumber();
 
     }
 }
