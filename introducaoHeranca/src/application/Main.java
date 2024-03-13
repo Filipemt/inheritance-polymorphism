978ff279644da60b46771ed4bc92622caed8dc54
Filipemtb <filipera3@gmail.com>
2024-03-13 15:54:50 -0300

    create: add SavingsAccount
    criando a classe SavingAccount e adicionando na main algumas "brincadeiras" para aprender sobre o Upcasting

--- introducaoHeranca/src/application/Main.java
+++ introducaoHeranca/src/application/Main.java
@@ -1,12 +1,23 @@
 package application;
 
+import entities.Account;
 import entities.BusinessAccount;
+import entities.SavingsAccount;
 
 public class Main {
     public static void main(String[] args) {
 
-        BusinessAccount account = new BusinessAccount();
-        account.getNumber();
+        Account acc = new Account(1001, "Filipe", 0.0);
+        BusinessAccount bacc = new BusinessAccount(1002, "Gustavo", 0.0, 500.00);
 
+        /*
+            UPCASTING
+            Pegando um objeto de alguma subclasse e atribuindo para uma variável
+            da super classe
+         */
+
+        Account acc1 = bacc;
+        Account acc2 = new BusinessAccount(1003, "Simone", 0.0, 200.00);
+        Account acc3 = new SavingsAccount(1004, "Maria", 200.0, 0.01);
     }
 }
