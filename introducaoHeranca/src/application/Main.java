2407555798724391f6f04bb436cb61d440faacf4
Filipemtb <filipera3@gmail.com>
2024-03-13 16:18:50 -0300

    feat: learning instanceof
     adicionando na main algumas "brincadeiras" para aprender sobre o uso de downCasting

--- introducaoHeranca/src/application/Main.java
+++ introducaoHeranca/src/application/Main.java
@@ -19,5 +19,27 @@ public class Main {
         Account acc1 = bacc;
         Account acc2 = new BusinessAccount(1003, "Simone", 0.0, 200.00);
         Account acc3 = new SavingsAccount(1004, "Maria", 200.0, 0.01);
+
+          /*
+            DOWNCASTING
+            Pegando um objeto de alguma superclasse e atribuindo para a subclasse.
+            processo inverso de Upcasting
+         */
+
+        BusinessAccount acc4 = (BusinessAccount) acc2; // Fazendo DownCasting manual
+        acc4.loan(100.0);
+
+        // BusinessAccount acc5 = (BusinessAccount) acc3; Objeto acc3 não é de BusinessAccount, então o downcasting não é possivel
+        if (acc3 instanceof BusinessAccount) { // fazendo validação e aprendendo sobre o intanceof
+            BusinessAccount acc5 = (BusinessAccount) acc3;
+            acc5.loan(200.0);
+            System.out.println("Loan!");
+        }
+
+        if (acc3 instanceof SavingsAccount) {
+            SavingsAccount acc5 = (SavingsAccount) acc3;
+            acc5.updateBalance();
+            System.out.println("Update!");
+        }
     }
 }
